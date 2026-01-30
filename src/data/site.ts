f2f36e8642213fc581a31d9dd80061b7cbec0821
Geri Gian Epanto <gianepanto@gmail.com>
2026-01-30 13:55:46 +0800

    feat: copy email animation, load in animation, refactor data

---
+++ src/data/site.ts
@@ -0,0 +1,19 @@
+export const SITE_DATA = {
+  name: 'giann.',
+  location: 'Cebu, Philippines',
+  email: 'epanto.gg@gmail.com',
+  socials: [
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/gian-epanto/',
+    },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/gian-gg',
+    },
+    {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/epanto.gg/',
+    },
+  ],
+};
