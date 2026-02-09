b51221400b04e128cb1d90ed13d1c739afb773a9
Geri Gian Epanto <gianepanto@gmail.com>
2026-02-09 10:53:10 +0800

    chore: update giann.dev URLs to www

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://giann.dev',
+  site: 'https://www.giann.dev',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
